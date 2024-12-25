41e8c7c0692f20da00b006b62603bd42873e5f3a
Матвей Суворов <matvey@iMac-Matvej.local>
2024-12-25 02:57:48 -0800

    _

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -11,14 +11,17 @@ const Header = () => {
         <div className={cl.header}>
             <h1 className='heading'>Soundest</h1>
             <ul className={cl.menu}>
-                <li className={cl.menu__item}><a href="/">зачем нужен Soundest</a></li>
-                <li className={cl.menu__item}><a href="#features">фишки сервиса</a></li>
+                <li className={cl.menu__item}><a href="#forwhat">зачем нужен Soundest</a></li>
                 <li className={cl.menu__item}><a href="#aboutme">о создателе</a></li>
             </ul>
-            <Fillbutton onClick={() => loginViaGoogle((login) => {
-                cookie.save('login', login, {path: '/', maxAge: 31536000})
-                router.push('/me')
-            })}>Войти</Fillbutton>
+            <div>
+                <a href="https://www.rustore.ru/catalog/app/com.ms.sndst" target='_blank' className={cl.download}>скачать приложение для Android</a>
+                <Fillbutton onClick={() => loginViaGoogle((login) => {
+                    cookie.save('login', login, {path: '/', maxAge: 31536000})
+                    router.push('/me')
+                })}>Войти</Fillbutton>
+            </div>
+            
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,22 +21,31 @@ const Main = () => {
             <div className={cl.def}>
                 <h1 className={cl.def__text}>Soundest - это облако для хранения и прослушивания музыки</h1>
             </div>
-            <div className={cl.infoBlock}>
+            <div className={cl.infoBlock} id="forwhat">
                 <h1 className="heading">зачем нужен Soundest</h1>
                 <p className={cl.info}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum laboriosam praesentium minima accusantium quos? Tempora non voluptates quidem, quae quibusdam sunt numquam debitis totam dolorum aut neque voluptas similique laboriosam?
+                Вы можете выгрузить всю свою скачанную музыку в Soundest, и она будет доступна на любом вашем устройстве (будь то это пк или телефон) c установленным Soundest. На этом же сервисе можно слушать музыку: как загруженную в облако, так и локальные mp3 файлы.<br /><br />
+
+Если с одним из ваших устройств что-то случится (например вы сломаете или потеряете телефон), с вашей музыкой ничего не случиться, так как она будет хранится в облаке, а не на телефоне. Вам достаточно лишь найти новый телефон, скачать Soundest, и вот вся ваша музыка снова вам доступна, ее не нужно заново скачивать.<br /><br />
+
+Soundest можно сравнить с Google Photo или Google Drive, только он для музыки.<br /><br />
+
+Есть как эта web версия для пк или ноутбука, так и <a href="https://www.rustore.ru/catalog/app/com.ms.sndst" target="blank" style={{textDecoration: 'underline'}}>приложение</a> для Android.<br />
                 </p>
             </div>
-            <div className={cl.infoBlock}>
-                <h1 className="heading">фишки сервиса</h1>
-                <p className={cl.info}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum laboriosam praesentium minima accusantium quos? Tempora non voluptates quidem, quae quibusdam sunt numquam debitis totam dolorum aut neque voluptas similique laboriosam?
-                </p>
-            </div>
-            <div className={cl.infoBlock}>
+            <div className={cl.infoBlock} id="aboutme">
                 <h1 className="heading">о создателе</h1>
                 <p className={cl.info}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum laboriosam praesentium minima accusantium quos? Tempora non voluptates quidem, quae quibusdam sunt numquam debitis totam dolorum aut neque voluptas similique laboriosam?
+                    Меня зовут Матвей, мне 16 лет, и я создатель Soundest.
+                    <br /><br />
+                    По одно время я пользовался Яндекс Музыкой с бесплатным пробным периодом.
+                    Мне очень понравилось то, что я мог слушать всю свою музыку как на телефоне (пока гуляю), так и на компьюьтере (когда прихожу домой и сажусь за компьютер).
+                    Но я понимал, что пробный период рано или поздно закончится, а кроссплатформенность музыки сохранить хочется. Поэтому я придумал создать сервис, с помощью
+                    которого я смогу легко иметь доступ ко всей своей музыке и с телефона, и с компьютера. Чтобы была такая кроссплатформенность я сделал веб версию Soundest (вы сейчс на ней)
+                    и мобильное приложение (можно скачать по ссылке в шапке сайта).
+                    <br /><br />
+                    О других моих проектах вы можете узнать на моем <a href="https://matvey.vercel.app" target="blank" style={{textDecoration: 'underline'}}>сайтe</a>.
+                    Также добро пожаловать на мой <a href="https://www.youtube.com/@matveysuvorov" target="blank" style={{textDecoration: 'underline'}}>ютуб канал</a> и <a href="https://www.youtube.com/@matveysuvorov" target="blank" style={{textDecoration: 'underline'}}>гитхаб</a>.
                 </p>
             </div>
         </>
